016695fdaca3c9616cef52432e15ad208db98fca
Karl Peterson <kbpeterson@gmail.com>
2017-03-28 15:46:17 -0700

    should fix production email urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "app_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'smtp.sendgrid.net' }
+  config.action_mailer.default_url_options = { host: 'points.bestdayever.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
