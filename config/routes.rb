3f3bdbe446dd03b1cd75a313f603a9b58baca97f
Karl Peterson <kbpeterson@gmail.com>
2017-03-31 10:09:47 -0700

    lots of updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # mount ForestLiana::Engine => '/forest'
   resources :challenges, :datapoints
   devise_for :users
+  authenticated :user do
+  	root to: 'challenges#dashboard', as: :authenticated_root
+  end
   root to: "application#index"
   get 'info',			to: 'application#info'
   get 'dashboard',		to: 'challenges#dashboard'
