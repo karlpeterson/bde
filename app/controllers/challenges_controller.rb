ea92423adbc4205b9a19adfa9672a64a337d50ac
Karl Peterson <kbpeterson@gmail.com>
2017-04-12 18:14:14 -0700

    fixed update methods

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -19,19 +19,26 @@ class ChallengesController < ApplicationController
 		user_challenges = @user.challenges
 		@current_challenge = user_challenges.where("DATE(?) BETWEEN start_date AND end_date", @today).last
 		# check user for current cohort
+		# current_cohort = Cohort.where(:challenge_id => @current_challenge.id)
+		
 		if @current_challenge.present?
 			@stat_list = Stat.where(:challenge_id => @current_challenge.id).order(:rank)
 		end
 	end
 
 	def index
-		@challenges = Challenge.all
+		user = current_user
+		if can? :edit, Challenge
+			@challenges = Challenge.all
+		else
+			@challenges = user.challenges
+		end
 	end
 
 	def show
+		@user = current_user
 		@challenge = Challenge.find(params[:id])
-		@datapoint_list = Datapoint.where("challenge_id = ? AND user_id = ?", params[:id], current_user.id).order(:day)
-		# @stat = Stat.where("challenge_id = ? AND user_id = ?", @current_challenge.id, current_user.id)
+		@stat_list = Stat.where(:challenge_id => @challenge.id).order(:rank)
 	end
 
 	def edit
@@ -76,11 +83,15 @@ class ChallengesController < ApplicationController
 				# for each date in range create an empty Datapoint
 				date_range.each_with_index do |day, index|
 					day_count = index + 1
-					datapoint = Datapoint.new(:user_id => user.id, :challenge_id => @challenge.id, :date => day, :day => day_count)
-					if datapoint.valid?
-						datapoint.save
+					if Datapoint.where(:user_id => user.id, :challenge_id => @challenge.id, :date => day, :day => day_count)
+						# do nothing if Datapoint already exists
 					else
-						@errors += datapoint.errors
+						datapoint = Datapoint.new(:user_id => user.id, :challenge_id => @challenge.id, :date => day, :day => day_count)
+						if datapoint.valid?
+							datapoint.save
+						else
+							@errors += datapoint.errors
+						end
 					end
 				end
 			end
@@ -103,6 +114,9 @@ class ChallengesController < ApplicationController
 			date_range	= start_date..end_date
 			date_range	= date_range.map(&:to_s)
 
+			stat_list = Stat.where(:challenge_id => @challenge.id)
+			stat_list.delete_all
+
 			# loop through all associated users
 			@user_list.each do |user|
 
@@ -120,8 +134,19 @@ class ChallengesController < ApplicationController
 						@errors += cohort.errors
 					end
 
-					#create join
-					stat = Stat.new(:user_id => user.id, :challenge_id => @challenge.id, :total_points => 0, :rank => 0)
+					#create join & update stats
+					datapoints = Datapoint.where(:user_id => user.id, :challenge_id => @challenge.id)
+					grand_total = datapoints.sum(:total_points)
+
+					stat_list = Stat.where(:challenge_id => @challenge.id).order(:total_points).reverse_order
+					rank = 1
+					stat_list.each_with_index do |stat, i|
+						stat_list[i-1].total_points == stat.total_points ? rank : rank = i+1
+						stat.rank = rank
+						stat.save
+					end
+
+					stat = Stat.new(:user_id => user.id, :challenge_id => @challenge.id, :total_points => grand_total, :rank => 0)
 					if stat.valid?
 						stat.save
 					else
@@ -131,11 +156,15 @@ class ChallengesController < ApplicationController
 					# for each date in range create an empty Datapoint
 					date_range.each_with_index do |day, index|
 						day_count = index + 1
-						datapoint = Datapoint.new(:user_id => user.id, :challenge_id => @challenge.id, :date => day, :day => day_count)
-						if datapoint.valid?
-							datapoint.save
+						if Datapoint.where(:user_id => user.id, :challenge_id => @challenge.id, :date => day, :day => day_count)
+							# do nothing if Datapoint already exists
 						else
-							@errors += datapoint.errors
+							datapoint = Datapoint.new(:user_id => user.id, :challenge_id => @challenge.id, :date => day, :day => day_count)
+							if datapoint.valid?
+								datapoint.save
+							else
+								@errors += datapoint.errors
+							end
 						end
 					end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    if user.admin?
+    if user.has_role? :admin
       can :manage, :all
     else
       can :read, :all
+      can :manage, Datapoint
+      can :manage, User
     end
   end
 end
