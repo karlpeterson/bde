2401fbcd4703e4541e5b30fa340313bd4ebabdb9
Karl Peterson <kbpeterson@gmail.com>
2017-04-13 17:58:28 -0700

    fixed Rankings error, added JS current day highlighting

--- app/assets/javascripts/bde.js
+++ app/assets/javascripts/bde.js
@@ -1,7 +1,28 @@
+"use strict";
+
+function parseDate(s) {
+  var b = s.split("-");
+  return new Date(b[0], --b[1], b[2]).getTime();
+}
+
 $(document).on('turbolinks:load', function() {
 	// Responsive Nav
   var mainNav = responsiveNav("#main-nav", {
     customToggle: "navToggle"
+  });
+
+  var today = new Date();
+	today.setHours(0,0,0,0);
+
+	// Highlight current day on Dashboard
+  $('#datapoint_list tbody tr').each(function(){
+  	var list_date = $(this).data('date');
+  	list_date = parseDate(list_date);
+
+  	if (list_date.valueOf() === today.valueOf()) {
+  		$(this).addClass('today');
+  	}
+
   });
   
 });
